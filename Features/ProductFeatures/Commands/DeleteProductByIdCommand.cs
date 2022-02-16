95b79813ecfeb743096c524f862f9dd894e26876
Bader Tawafsheh <68567544+badertawafsheh@users.noreply.github.com>
2022-02-16 14:27:13 +0200

    Mapper DeleteProduct

--- Features/ProductFeatures/Commands/DeleteProductByIdCommand.cs
+++ Features/ProductFeatures/Commands/DeleteProductByIdCommand.cs
@@ -1,4 +1,6 @@
-﻿using CQRS_With_MeditR_Demo.Data;
+﻿using AutoMapper;
+using CQRS_With_MeditR_Demo.Data;
+using CQRS_With_MeditR_Demo.DTO;
 using CQRS_With_MeditR_Demo.Model;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -8,24 +10,26 @@ using System.Threading.Tasks;
 
 namespace CQRS_With_MeditR_Demo.Features.ProductFeatures.Commands
 {
-    public class DeleteProductByIdCommand : IRequest<Product>
+    public class DeleteProductByIdCommand : IRequest<GetProductDTO>
     {
         public int Id { get; set; }
-        public class DeleteProductByIdCommandHandler : IRequestHandler<DeleteProductByIdCommand, Product>
+        public class DeleteProductByIdCommandHandler : IRequestHandler<DeleteProductByIdCommand, GetProductDTO>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public DeleteProductByIdCommandHandler(DataContext context)
+            public DeleteProductByIdCommandHandler(DataContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
-            public async Task<Product> Handle(DeleteProductByIdCommand request, CancellationToken cancellationToken)
+            public async Task<GetProductDTO> Handle(DeleteProductByIdCommand request, CancellationToken cancellationToken)
             {
                 var product = await _context.Products.Where(a => a.Id == request.Id).FirstOrDefaultAsync();
                 if (product == null) return null;
                 _context.Products.Remove(product);
                 await _context.SaveChangesAsync();
-                return product;
+                return _mapper.Map<GetProductDTO>(product);
             }
         }
     }
