8d9946923bb49494843c87e505d839163171cce9
Bader Tawafsheh <68567544+badertawafsheh@users.noreply.github.com>
2022-02-16 10:27:36 +0200

    Mapper getProductById

--- Features/ProductFeatures/Queries/GetProductByIdQuery.cs
+++ Features/ProductFeatures/Queries/GetProductByIdQuery.cs
@@ -1,4 +1,6 @@
-﻿using CQRS_With_MeditR_Demo.Data;
+﻿using AutoMapper;
+using CQRS_With_MeditR_Demo.Data;
+using CQRS_With_MeditR_Demo.DTO;
 using CQRS_With_MeditR_Demo.Model;
 using MediatR;
 using System.Linq;
@@ -7,24 +9,26 @@ using System.Threading.Tasks;
 
 namespace CQRS_With_MeditR_Demo.Features.ProductFeatures.Queries
 {
-    public class GetProductByIdQuery:IRequest<Product>
+    public class GetProductByIdQuery:IRequest<GetProductDTO>
     {
         public int Id { get; set; }
 
-        public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery,Product>
+        public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, GetProductDTO>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public GetProductByIdQueryHandler(DataContext context)
+            public GetProductByIdQueryHandler(DataContext context,IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
-            public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+            public async Task<GetProductDTO> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
             {
                 var product = _context.Products.Where(a => a.Id == request.Id).FirstOrDefault();
                 if (product == null) return null;
                 
-                return product; 
+                return _mapper.Map<GetProductDTO>(product); 
             }
         }
     }
